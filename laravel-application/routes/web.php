ab577410b5c2d078826a1ee2bc888ce816dc4ede
Craig Dennis <zephni@hotmail.co.uk>
2022-12-05 17:51:43 +0000

    Temporary routes for local ENV, testing etc

--- laravel-application/routes/web.php
+++ laravel-application/routes/web.php
@@ -2,32 +2,32 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+/* Temporary routes for testing purposes (will only work if env is local)
+----------------------------------------------------------------*/
+if (env('APP_ENV') === 'local') {
+    Route::group(['prefix' => 'temp'], function(){
+        Route::get('/set-permissions', function () {
+            $user = App\Models\User::where('email', 'zephni@hotmail.co.uk')->first();
+            $user->permissions = [
+                'admin'  => true, // admin means can access admin section, if authenticated but admin is false, then this application allows general authenticated users
+                'master' => true, // master means can do anything, may want to add more granular permissions later
+                // examples of granular permissions:
+                // 'tables.create' => ['table_name', ...],
+                // 'tables.update' => ['table_name', ...],
+                // 'tables.delete' => ['table_name', ...],
+            ];
+            $user->save();
+            dd($user->permissions);
+        });
+    });
+}
 
-// Temporary route to modify permissions to zephni user
-Route::get('/set-permissions', function () {
-    $user = App\Models\User::where('email', 'zephni@hotmail.co.uk')->first();
-    $user->permissions = [
-        'admin'     => true, // admin means can log in to admin section
-        'master'    => true, // master means can do anything, may want to add more granular permissions later
-    ];
-    $user->save();
-    dd($user->permissions);
-});
-
-// Frontend routes
+/* Frontend routes
+----------------------------------------------------------------*/
 Route::view('/', 'home')->name('home');
 
-// Auth routes
+/* Auth routes
+----------------------------------------------------------------*/
 Route::controller(App\Http\Controllers\AuthController::class)->group(function () {
     Route::middleware('redirect_if_authenticated')->group(function(){
         // Login
@@ -43,7 +43,8 @@ Route::controller(App\Http\Controllers\AuthController::class)->group(function ()
     Route::get('/logout', 'logout')->name('logout');
 });
 
-// Admin routes
+/* Admin routes
+----------------------------------------------------------------*/
 Route::middleware(['auth', 'is_admin'])->prefix('admin')->group(function () {
     Route::get('/', function () {return 'Logged in as admin';})->name('admin-home');
 });
