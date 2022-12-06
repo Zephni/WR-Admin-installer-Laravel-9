971d2827e12eb08bf536f0660188f232d480d988
Craig Dennis <zephni@hotmail.co.uk>
2022-12-06 14:15:47 +0000

    Fully commented AuthController

--- laravel-application/app/Http/Controllers/AuthController.php
+++ laravel-application/app/Http/Controllers/AuthController.php
@@ -13,7 +13,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
-
     /**
      * register
      * Register a new user (post request)
@@ -22,20 +21,26 @@ class AuthController extends Controller
      */
     public function register(Request $request): Response
     {
+        // Validate request
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8|confirmed',
         ]);
 
+        // Create a new user
         $user = new User();
         $user->name = $request->name;
         $user->email = $request->email;
         $user->password = Hash::make($request->password);
+        $user->permissions = [
+            'master' => false,
+            'admin'  => true
+        ];
         $user->save();
 
+        // Login the user and redirect to home page
         Auth::login($user);
-
         return redirect()->route('home');
     }
 
@@ -47,22 +52,27 @@ class AuthController extends Controller
      */
     public function login(Request $request): Response
     {
+        // Validate request
         $request->validate([
             'email' => 'required|email',
             'password' => 'required'
         ]);
 
+        // Gather the email and password from the request
         $credentials = $request->only('email', 'password');
 
+        // Attempt to login the user and redirect to home page if successful, otherwise back to login page with error
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
 
             return redirect()->route('home');
         }
-
-        return back()->withErrors([
-            'message' => 'The provided credentials do not match our records.',
-        ]);
+        else
+        {
+            return back()->withErrors([
+                'message' => 'The provided credentials do not match our records.',
+            ]);
+        }
     }
 
 
@@ -74,12 +84,17 @@ class AuthController extends Controller
      */
     public function forgot_password(Request $request): Response
     {
-        $request->validate(['email' => 'required|email']);
+        // Validate request
+        $request->validate([
+            'email' => 'required|email'
+        ]);
 
+        // Sends the reset link to the user
         $status = Password::sendResetLink(
             $request->only('email')
         );
 
+        // Redirects to login page with email if successful, otherwise back to forgot password page with error
         return $status === Password::RESET_LINK_SENT
                 ? back()->with(['status' => __($status)])
                 : back()->withErrors(['email' => __($status)]);
@@ -93,14 +108,16 @@ class AuthController extends Controller
      */
     public function reset_password(Request $request): Response
     {
+        // Validate request
         $request->validate([
             'token' => 'required',
             'email' => 'required|email',
             'password' => 'required|min:8|confirmed',
         ]);
 
+        // Resets the password of the user based on the token, email, password and password confirmation
         $status = Password::reset(
-            $request->only('email', 'password', 'password_confirmation', 'token'),
+            $request->only('token', 'email', 'password', 'password_confirmation'),
             function ($user, $password) {
                 $user->forceFill([
                     'password' => Hash::make($password)
@@ -112,6 +129,7 @@ class AuthController extends Controller
             }
         );
 
+        // Redirects to login page with email if successful, otherwise back to reset page with error
         return $status === Password::PASSWORD_RESET
                 ? redirect()->route('login')->with(['status' => __($status), 'email' => $request->email])
                 : back()->withErrors(['email' => [__($status)]]);
@@ -126,12 +144,14 @@ class AuthController extends Controller
      */
     public function logout(Request $request): Response
     {
+        // Logout the user
         Auth::logout();
 
+        // Invalidate the session and regenerate the CSRF token
         $request->session()->invalidate();
-
         $request->session()->regenerateToken();
 
+        // Redirect to home page
         return redirect()->route('home');
     }
 }
